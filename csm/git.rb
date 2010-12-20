892a7c8eeb1109df29c52045afb1065d03f2fdc5
Kirill Radzikhovskyy <kirillrdy@silverpond.com.au>
2010-12-20 13:59:54 +1100

    Small updates

--- csm/git.rb
+++ csm/git.rb
@@ -3,7 +3,7 @@ package 'git' do
   type 'Make'
   source "http://kernel.org/pub/software/scm/git/git-1.6.6.1.tar.bz2"
 
-# TODO fix this patch, colors should be on by default
-#@patches = "http://files.kita-linux.org/patches/git-colorful.patch"
+  # TODO fix this patch, colors should be on by default
+  #patch  "http://files.kita-linux.org/patches/git-colorful.patch"
 
 end
