9df24e66a5c5348f64e09aff22352307b10eda3f
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-02-27 16:44:50 +1100

    Added linux kernel

---
+++ kita-linux/linux.rb
@@ -0,0 +1,22 @@
+package 'linux' do
+  source 'http://www.kernel.org/pub/linux/kernel/v2.6/linux-2.6.37.2.tar.bz2'
+
+  configure do
+    '
+    make mrproper
+    make LANG=<host_LANG_value> LC_ALL= menuconfig
+    '
+  end
+
+  build do
+    '
+    make
+    make modules_install
+    '
+  end
+
+  install do
+    'cp -v arch/x86/boot/bzImage /boot/kernel-2.6.37'
+  end
+
+end
