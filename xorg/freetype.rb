d74336b59decf3e9084c076afc2dc97ad826a185
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-03-01 22:12:01 +1100

    possibly fixed something

--- xorg/freetype.rb
+++ xorg/freetype.rb
@@ -3,11 +3,11 @@ package 'freetype' do
   source "http://downloads.sourceforge.net/freetype/freetype-2.4.4.tar.bz2"
 
   configure do
-    "
+    <<-EOF
     sed -i -r -e 's:.*(#.*BYTE.*) .*:\1:' \
               -e 's:.*(#.*SUBPIX.*) .*:\1:' \
               include/freetype/config/ftoption.h &&
     ./configure --prefix=/usr
-    "
+    EOF
   end
 end
