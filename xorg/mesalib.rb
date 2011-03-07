9d30431bb3daaa19e57b05f284e9a8762822403f
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-03-07 19:46:49 +1100

    name change

--- xorg/mesalib.rb
+++ xorg/mesalib.rb
@@ -1,4 +1,4 @@
-package 'libmesa' do
+package 'mesalib' do
   type :make
   source 'ftp://ftp.freedesktop.org/pub/mesa/7.9/MesaLib-7.9.tar.bz2'
   depends_on ['xorg-lib','xorg-util','libdrm','expat','talloc']
