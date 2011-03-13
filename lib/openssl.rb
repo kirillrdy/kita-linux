b05c853d3dbd9e5b7eb60b3f4000c138bc771f83
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-03-13 12:04:13 +1100

    Meta for xorg font

---
+++ lib/openssl.rb
@@ -0,0 +1,4 @@
+package 'openssl' do
+  type :make
+  source 'http://www.openssl.org/source/openssl-1.0.0b.tar.gz'
+end
--- xorg/xorg.rb
+++ xorg/xorg.rb
@@ -6,7 +6,7 @@ def get_list_of_files url
 end
 
 
-def xorg_module module_name, xorg_version , additional_dependencies = []
+def xorg_module module_name, xorg_version , additional_dependencies = [], optional_post_install = nil
 
   xorg_files = get_list_of_files "http://anduin.linuxfromscratch.org/files/BLFS/svn/xorg/#{module_name}-#{xorg_version}.wget"
   xorg_base_url = "http://xorg.freedesktop.org/releases/individual/#{module_name}/"
@@ -21,6 +21,13 @@ def xorg_module module_name, xorg_version , additional_dependencies = []
     type :meta
     depends_on additional_dependencies + xorg_files.map{|x| File.smart_basename x }
     version xorg_version
+    
+    if optional_post_install
+      post_install do
+        optional_post_install
+      end
+    end
+
   end
 
 end
@@ -32,8 +39,7 @@ xorg_module 'proto','7.6-1'
 xorg_module 'util','7.6-1'
 xorg_module 'lib','7.6-1', ['fontconfig','xorg-proto','xorg-util','libXdmcp','libxcb']
 xorg_module 'app','7.6-1', ['libpng','xcb-util','xorg-lib','mesalib']
-xorg_module 'font','7.6-1', ['xorg-app','xcursor-themes']
-#TODO add symlink as per blfs font info
+xorg_module 'font','7.6-1', ['xorg-app','xcursor-themes'], 'install -v -d -m755 /usr/share/fonts && ln -svn /usr/share/fonts/X11/fonts/OTF /usr/share/fonts/X11-OTF && ln -svn /usr/share/fonts/X11/fonts/TTF /usr/share/fonts/X11-TTF'
 
 
 #package 'xorg' do
