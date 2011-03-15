26c4caca0446c408333caa1ed347ed3cac4a5f3a
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-03-15 19:41:47 +1100

    fixed openssl

--- lib/openssl.rb
+++ lib/openssl.rb
@@ -2,8 +2,9 @@ package 'openssl' do
   type :make
   source 'http://www.openssl.org/source/openssl-1.0.0b.tar.gz'
   configure do
-  '
-  ./config --prefix=/usr --openssldir=/etc/ssl shared zlib-dynamic
-  '
+  './config --prefix=/usr --openssldir=/etc/ssl shared zlib-dynamic'
+  end
+  build do
+    'MAKEFLAGS=-j1 make'
   end
 end
