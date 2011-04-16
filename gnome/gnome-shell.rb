fbfb1530ee901cc64930790acde5d05ecc3363bc
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-04-16 14:57:47 +1000

    added polkit

--- gnome/gnome-shell.rb
+++ gnome/gnome-shell.rb
@@ -1,9 +1,10 @@
 package 'gnome-shell' do
   type :make
   source 'http://ftp.gnome.org/pub/GNOME/sources/gnome-shell/3.0/gnome-shell-3.0.0.2.tar.bz2'
-  depends_on ["evolution-data-server","gobject-introspection","mutter","gjs","dconf","GConf",
-   "glib","gnome-desktop-3","gsettings-desktop-schemas", "gnome-icon-theme",
-   "gnome-icon-theme-symbolic","libcanberra","gnome-settings-daemon", "telepathy-glib", "telepathy-logger"]
+  depends_on ['GConf',"gobject-introspection","mutter","gjs","dconf",
+   "gnome-desktop-3","gsettings-desktop-schemas", "gnome-icon-theme",
+   "gnome-icon-theme-symbolic","libcanberra","gnome-settings-daemon", "telepathy-glib", "telepathy-logger",
+   "evolution-data-server"]
 end
 
 
---
+++ sys/polkit.rb
@@ -0,0 +1,18 @@
+package 'polkit' do
+  type :make
+  source 'http://hal.freedesktop.org/releases/polkit-0.100.tar.gz'
+  depends_on 'Linux-PAM'
+  configure do
+    <<-EOS
+      groupadd -f -g 28 polkituser
+
+      if [ `id -u polkituser` = '28' ]; then
+        echo "Polkit user already exists"
+      else
+        useradd -c "Policy Kit Daemon User" -d /dev/null -u 28 -g polkituser -s /bin/false polkituser
+      fi
+
+      ./configure --prefix=/usr --sysconfdir=/etc --libexecdir=/usr/lib/polkit-1 --localstatedir=/var
+    EOS
+  end
+end
