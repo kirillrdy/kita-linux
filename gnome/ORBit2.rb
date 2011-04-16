5fea7b5d4382b1e855a9fe61192b637cca1d81eb
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-04-16 12:56:45 +1000

    Long work on gnome begins

---
+++ gnome/ORBit2.rb
@@ -0,0 +1,9 @@
+package 'ORBit2' do
+  type :make
+  source 'http://ftp.gnome.org/pub/gnome/sources/ORBit2/2.14/ORBit2-2.14.19.tar.bz2'
+  #DEPEND="libIDL"
+#config_src()
+#{
+#./configure --prefix=/usr --sysconfdir=/etc
+#}
+end
