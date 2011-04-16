2169242b0d02c43ab9ffae769eaf2c1b28a3fb55
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-04-16 13:50:24 +1000

    More new packages

--- gnome/ORBit2.rb
+++ gnome/ORBit2.rb
@@ -1,7 +1,7 @@
 package 'ORBit2' do
   type :make
   source 'http://ftp.gnome.org/pub/gnome/sources/ORBit2/2.14/ORBit2-2.14.19.tar.bz2'
-  #DEPEND="libIDL"
+  depends_on 'libIDL'
 #config_src()
 #{
 #./configure --prefix=/usr --sysconfdir=/etc
---
+++ lib/libIDL.rb
@@ -0,0 +1,5 @@
+package 'libIDL' do
+  type :make
+  depends_on 'glib'
+  source 'http://ftp.gnome.org/pub/gnome/sources/libIDL/0.8/libIDL-0.8.14.tar.bz2'
+end
