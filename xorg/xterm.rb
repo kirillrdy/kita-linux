2be96d171000fc91344e038fe2ddb664ff07475b
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-04-16 11:11:16 +1000

    Removed old stuff and ported xterm

---
+++ xorg/xterm.rb
@@ -0,0 +1,25 @@
+package 'xterm' do
+  type :make
+  source 'ftp://invisible-island.net/xterm/xterm-267.tgz'
+  depends_on 'xorg-app'
+  configure do
+    <<-EOS
+    sed -i '/v0/,+1s/new:/new:kb=^?:/' termcap &&
+echo -e '\tkbs=\\177,' >>terminfo &&
+TERMINFO=/usr/lib/terminfo ./configure=/usr \
+    --enable-luit --enable-wide-chars \
+    --with-app-defaults=/etc/X11/app-defaults
+    EOS
+  end
+  install do
+    <<-EOS
+cat >> /etc/X11/app-defaults/XTerm << "EOF"
+*VT100*locale: true
+*VT100*faceName: Monospace
+*VT100*faceSize: 10
+*backarrowKeyIsErase: true
+*ptyInitialErase: true
+EOF
+    EOS
+  end
+end
