97446fb41997879291bb3f5a5f1895ed4c4af103
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-04-23 13:55:40 +1000

    added source for GConf

--- gnome/GConf.rb
+++ gnome/GConf.rb
@@ -1,6 +1,7 @@
 package 'GConf' do
   type :make
   depends_on ['ORBit2','libxml2','gtk+', 'polkit']
+  source 'ftp://ftp.gnome.org/pub/gnome/sources/GConf/2.32/GConf-2.32.2.tar.bz2'
   configure do
   '
   ./configure --prefix=/usr \
