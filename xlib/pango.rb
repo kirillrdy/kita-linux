c140c2d0898d03907f8dbb969d09adf727de31b4
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-04-24 13:05:08 +1000

    forgot to remove old pango

--- xlib/pango.rb
+++
@@ -1,8 +0,0 @@
-package 'pango' do
-  type :make
-  source 'http://ftp.gnome.org/pub/gnome/sources/pango/1.28/pango-1.28.2.tar.bz2'
-  depends_on ['glib','cairo','xorg','fontconfig']
-  post_install do
-    'pango-querymodules > /etc/pango/pango.modules'
-  end
-end
